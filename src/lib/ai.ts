f5074d473b442f89faf5d0d256b0c8d953eac616
Agam More <agam@agam.me>
2023-05-27 02:13:45 +0900

    Debug prompt

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -9,7 +9,7 @@ export async function getBugs(
   const messages: ChatCompletionRequestMessage[] = [
     {
       role: "user",
-      content: `You are a senior code reviewer capable of understanding complex code and spotting potential bugs. Your role involves identifying serious bugs, which could include logic errors, performance bugs, security bugs, race conditions, and calculation bugs. Please ensure that your analysis is thorough and avoids false positives. Here is some ${lang} code. Please review it and identify any serious bugs you find. Remember, we're not looking for stylistic issues, but real bugs that could affect the functionality or security of the application. If you aren't certain about a bug, it's better to return an empty list. When you find a bug, please provide a short reason and its type. The result should be returned in the following valid JSON format: [{"line":1,"reason":"short reason", "type":"type"},...]\n\`\`\`\n${sourceCode}\`\`\``,
+      content: `You are a senior code reviewer capable of understanding complex code and spotting potential bugs. Your role involves identifying serious bugs, which could include logic errors, performance bugs, security bugs, race conditions, and calculation bugs. Please ensure that your analysis is thorough and avoids false positives. Here is some ${lang} code. Remember, we're not looking for stylistic issues, but real bugs that could affect the functionality or security of the application. If you aren't certain about a bug, it's better to return an empty list. When you find a bug, please provide a short reason and its type. The result should be only valid JSON format: [{"line":1,"reason":"short reason", "type":"type"},...]\n\`\`\`\n${sourceCode}\`\`\`\n JSON Result:\n`,
     },
   ];
 
