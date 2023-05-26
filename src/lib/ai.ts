a8aa26ba1effaf5b249ca486dd8574eab0a492c0
Agam More <agam@agam.me>
2023-05-27 01:01:25 +0900

    Debug ai

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -8,13 +8,8 @@ export async function getBugs(
   // Construct the chat messages
   const messages: ChatCompletionRequestMessage[] = [
     {
-      role: "system",
-      content:
-        "You are a knowledgeable code reviewer capable of understanding complex code and spotting potential bugs. Your role involves identifying serious bugs, which could include logic errors, performance bugs, security bugs, race conditions, and calculation bugs. Please ensure that your analysis is thorough and avoids false positives.",
-    },
-    {
       role: "user",
-      content: `Here is some ${lang} code. Please review it and identify any serious bugs you find. Remember, we're not looking for stylistic issues, but real bugs that could affect the functionality or security of the application. If you aren't certain about a bug, it's better to return an empty list. When you find a bug, please provide a short reason and its type. The result should be returned in the following JSON format: [{"line":1,"reason":"short reason", "type":"type"},...]\n\`\`\`${lang}\n${sourceCode}\`\`\``,
+      content: `You are a knowledgeable code reviewer capable of understanding complex code and spotting potential bugs. Your role involves identifying serious bugs, which could include logic errors, performance bugs, security bugs, race conditions, and calculation bugs. Please ensure that your analysis is thorough and avoids false positives. Here is some ${lang} code. Please review it and identify any serious bugs you find. Remember, we're not looking for stylistic issues, but real bugs that could affect the functionality or security of the application. If you aren't certain about a bug, it's better to return an empty list. When you find a bug, please provide a short reason and its type. The result should be returned in the following JSON format: [{"line":1,"reason":"short reason", "type":"type"},...]\n\`\`\`\n${sourceCode}\`\`\``,
     },
   ];
 
@@ -25,6 +20,7 @@ export async function getBugs(
     max_tokens: 128,
   });
 
+  console.log("DEBUG", completion.data);
   const res = completion.data?.choices[0]?.message?.content ?? "[]";
   let resultParsed = [];
   try {
