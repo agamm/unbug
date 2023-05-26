6190ae3ba534899959fecef9bbf6cef548292b39
Agam More <agam@agam.me>
2023-05-27 02:31:49 +0900

    Fix ai temprature

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -9,7 +9,7 @@ export async function getBugs(
   const messages: ChatCompletionRequestMessage[] = [
     {
       role: "user",
-      content: `You are a senior code reviewer capable of understanding complex code and spotting potential bugs. Your role involves identifying serious bugs, which could include logic errors, performance bugs, security bugs, race conditions, and calculation bugs. Please ensure that your analysis is thorough and avoids false positives. Here is some ${lang} code. Remember, we're not looking for stylistic issues, but real bugs that could affect the functionality or security of the application. If you aren't certain about a bug, it's better to return an empty list. When you find a bug, please provide a short reason and its type. The result should be only valid JSON format: [{"line":1,"reason":"short reason", "type":"type"},...]\n\`\`\`\n${sourceCode}\`\`\`\n JSON Result:\n`,
+      content: `You are a senior code reviewer capable of understanding complex code and spotting potential bugs. Your role involves identifying serious bugs, which could include logic errors, performance bugs, security bugs, race conditions, and calculation bugs. Please ensure that your analysis is correct and avoids false positives. Here is some ${lang} code. Remember, we're not looking for stylistic issues, but real bugs that could affect the functionality or security of the application. If you aren't certain about a bug, it's better to return an empty list. When you find a bug, please provide a short reason and its type. The result should be only valid JSON format: [{"line":1,"reason":"short reason", "type":"type"},...]\n\`\`\`\n${sourceCode}\`\`\`\n JSON Result:\n`,
     },
   ];
 
@@ -18,6 +18,7 @@ export async function getBugs(
     model: "gpt-3.5-turbo",
     messages: messages,
     max_tokens: 128,
+    temperature: 0,
   });
 
   const res = completion.data?.choices[0]?.message?.content ?? "[]";
