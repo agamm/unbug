77dd1366c77fcecf7a8c6cff44f45651c67d4fc5
Agam More <agam@agam.me>
2023-06-04 23:41:51 +0900

    Fix prompt

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -5,7 +5,7 @@ export async function getBugs(openai: OpenAIApi, sourceCode: string) {
   const messages: ChatCompletionRequestMessage[] = [
     {
       role: "user",
-      content: `You are a senior developer. Check the code for bugs (logic, security, calculation or race condition bugs). If you aren't sure there is a bug return an empty list. The result should be only valid JSON format: [{"line":1,"reason":"short reason", "type":"type"},...]\n\`\`\`\n${sourceCode}\`\`\`\n JSON Result:\n`,
+      content: `You are a senior developer. Check the code for bugs (logic, security, calculation or race condition bugs). Think of how the code would run and where it might have a serious bug and edge case. If you aren't sure there is a bug return an empty list. The result should be only valid JSON format: [{"line":1,"reason":"short reason", "type":"type"},...]\n\`\`\`\n${sourceCode}\`\`\`\n JSON Result:\n`,
     },
   ];
 
